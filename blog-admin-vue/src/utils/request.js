37a526d3fe979195c2e892e78ef3e0efef8b555e
Modeest <285561932@qq.com>
2019-01-18 00:40:48 +0800

    build挂CDN

--- blog-admin-vue/src/utils/request.js
+++ blog-admin-vue/src/utils/request.js
@@ -13,13 +13,13 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+      config.headers.Authorization = `Bearer ${getToken()}` // 让每个请求携带自定义token 请根据实际情况自行修改
     }
     return config
   },
   error => {
     // Do something with request error
-    console.log(error) // for debug
+    console.log('request拦截器：', error) // for debug
     Promise.reject(error)
   }
 )
@@ -28,18 +28,18 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => {
     /**
-     * code为非20000是抛错 可结合自己业务进行修改
+     * errcode为非20000是抛错 可结合自己业务进行修改
      */
     const res = response.data
-    if (res.code !== 20000) {
+    if (res.errcode !== 10000) {
       Message({
-        message: res.message,
+        message: res.errmsg,
         type: 'error',
         duration: 5 * 1000
       })
 
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.errcode === 50008 || res.errcode === 50012 || res.errcode === 50014) {
         MessageBox.confirm(
           '你已被登出，可以取消继续留在该页面，或者重新登录',
           '确定登出',
@@ -62,7 +62,7 @@ service.interceptors.response.use(
   error => {
     console.log('err' + error) // for debug
     Message({
-      message: error.message,
+      message: error.errmsg,
       type: 'error',
       duration: 5 * 1000
     })
--- blog-admin-vue/vue.config.js
+++ blog-admin-vue/vue.config.js
@@ -1,10 +1,54 @@
 // Explanation of each configuration item You can find it in https://cli.vuejs.org/config/
 
 const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 function resolve(dir) {
   return path.join(__dirname, './', dir)
 }
+
+// cdn预加载使用
+const externals = {
+  'vue': 'Vue',
+  'vue-router': 'VueRouter',
+  'vuex': 'Vuex',
+  'axios': 'axios',
+  'element-ui': 'ELEMENT',
+  'js-cookie': 'Cookies',
+ 'nprogress': 'NProgress'
+}
+
+const cdn = {
+  // 开发环境
+  dev: {
+    css: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+      'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
+    ],
+    js: []
+  },
+  // 生产环境
+  build: {
+    css: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css',
+      'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
+    ],
+    js: [
+      'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
+      'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
+      'https://cdn.jsdelivr.net/npm/vuex@3.0.1/dist/vuex.min.js',
+      'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js',
+      'https://unpkg.com/element-ui/lib/index.js',
+      'https://cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js',
+      'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js'
+    ]
+  }
+}
+
+// 是否使用gzip
+const productionGzip = true
+// 需要gzip压缩的文件后缀
+const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
   /**
@@ -30,16 +74,56 @@ module.exports = {
     // proxy: {}
   },
 
-  configureWebpack: {
-    // We provide the app's title in Webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: 'vue-admin-template',
-    resolve: {
-      alias: {
-        '@': resolve('src')
+  // configureWebpack: {
+  //   // We provide the app's title in Webpack's name field, so that
+  //   // it can be accessed in index.html to inject the correct title.
+  //   name: '俊哥博客管理后台',
+  //   resolve: {
+  //     alias: {
+  //       '@': resolve('src')
+  //     }
+  //   }
+  // },
+  configureWebpack: config => {
+    const myConfig = {}
+    if (process.env.NODE_ENV === 'production') {
+      // 1. 生产环境npm包转CDN
+      myConfig.externals = externals
+
+      myConfig.plugins = []
+      // 2. 构建时开启gzip，降低服务器压缩对CPU资源的占用，服务器也要相应开启gzip
+      productionGzip && myConfig.plugins.push(
+        new CompressionWebpackPlugin({
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 8192,
+          minRatio: 0.8
+        })
+      )
+    }
+    if (process.env.NODE_ENV === 'development') {
+      /**
+       * 关闭host check，方便使用ngrok之类的内网转发工具
+       */
+      myConfig.devServer = {
+        disableHostCheck: true
       }
     }
+    //   open: true,
+    //   hot: true
+    //   // https: true,
+    //   // proxy: {
+    //   //   '/proxy': {
+    //   //     target: 'http://47.94.138.75',
+    //   //     // changeOrigin: true,
+    //   //     pathRewrite: {
+    //   //       '^/proxy': ''
+    //   //     }
+    //   //   }
+    //   // },
+    // }
+    return myConfig;
   },
+
   chainWebpack: config => {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
@@ -93,5 +177,18 @@ module.exports = {
           config.optimization.runtimeChunk('single')
         }
       )
+
+    /**
+     * 添加CDN参数到htmlWebpackPlugin配置中， 详见public/index.html 修改
+     */
+    config.plugin('html').tap(args => {
+      if (process.env.NODE_ENV === 'production') {
+        args[0].cdn = cdn.build
+      }
+      if (process.env.NODE_ENV === 'development') {
+        args[0].cdn = cdn.dev
+      }
+      return args
+    })
   }
 }
