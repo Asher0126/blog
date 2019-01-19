5b5d7f545cfe9479f5f9c647e6747465e2c1fa8a
Modeest <285561932@qq.com>
2019-01-19 15:24:38 +0800

    mod eslint and navbar

--- blog-admin-vue/.eslintrc.js
+++ blog-admin-vue/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
       }
     }],
     'vue/name-property-casing': ['error', 'PascalCase'],
+    // 'vue/require-component-is': [0],
     'accessor-pairs': 2,
     'arrow-spacing': [2, {
       'before': true,
--- blog-admin-vue/src/views/layout/components/Navbar.vue
+++ blog-admin-vue/src/views/layout/components/Navbar.vue
@@ -4,17 +4,17 @@
     <breadcrumb />
     <el-dropdown class="avatar-container" trigger="click">
       <div class="avatar-wrapper">
-        <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+        <img :src="avatar" class="user-avatar">
         <i class="el-icon-caret-bottom"/>
       </div>
       <el-dropdown-menu slot="dropdown" class="user-dropdown">
         <router-link class="inlineBlock" to="/">
           <el-dropdown-item>
-            Home
+            主页
           </el-dropdown-item>
         </router-link>
         <el-dropdown-item divided>
-          <span style="display:block;" @click="logout">LogOut</span>
+          <span style="display:block;" @click="logout">退出</span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
@@ -38,10 +38,10 @@ export default {
     ])
   },
   methods: {
-    toggleSideBar() {
+    toggleSideBar () {
       this.$store.dispatch('ToggleSideBar')
     },
-    logout() {
+    logout () {
       this.$store.dispatch('LogOut').then(() => {
         location.reload() // 为了重新实例化vue-router对象 避免bug
       })
